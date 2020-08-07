74f4d34241529f5c25dd7533f8b724d02dcff3d8
LennyDennis <lennydennis@gmail.com>
2020-08-08 01:32:53 +0300

    Add navigation view listner to navigate to different fragments

--- app/src/main/java/com/lennydennis/androidnavigation/MainActivity.java
+++ app/src/main/java/com/lennydennis/androidnavigation/MainActivity.java
@@ -5,38 +5,84 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.preference.PreferenceManager;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.lennydennis.androidnavigation.ui.AgreementFragment;
 import com.lennydennis.androidnavigation.ui.HomeFragment;
 import com.lennydennis.androidnavigation.ui.MessagesFragment;
 import com.lennydennis.androidnavigation.ui.SavedConnectionFragment;
+import com.lennydennis.androidnavigation.ui.SettingsFragment;
 import com.lennydennis.androidnavigation.util.PreferencesKey;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, NavigationView.OnNavigationItemSelectedListener {
 
     private BottomNavigationView mBottomNavigationView;
+    private ImageView mHeaderImageView;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
         mBottomNavigationView = findViewById(R.id.bottom_navigation);
         mBottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        View headerview = navigationView.getHeaderView(0);
+        mHeaderImageView = headerview.findViewById(R.id.header_image);
+
         isFirstLogin();
         initializeFragment();
+        setNavigationViewListener();
+        setHeaderImage();
 
     }
+
+    private void setHeaderImage() {
+        Glide.with(this)
+                .load(R.drawable.couple)
+                .into(mHeaderImageView);
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.bottom_nav_home:{
+        switch (item.getItemId()) {
+            case R.id.home: {
+                initializeFragment();
+                break;
+            }
+            case R.id.settings: {
+                SettingsFragment settingsFragment = new SettingsFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.main_content_frame, settingsFragment, getString(R.string.tag_fragment_home));
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                item.setChecked(true);
+                break;
+            }
+            case R.id.agreement: {
+                AgreementFragment agreementFragment = new AgreementFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.main_content_frame, agreementFragment, getString(R.string.tag_fragment_home));
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                break;
+            }
+            case R.id.bottom_nav_home: {
                 HomeFragment homeFragment = new HomeFragment();
                 FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                 fragmentTransaction.replace(R.id.main_content_frame, homeFragment, getString(R.string.tag_fragment_home));
@@ -45,7 +91,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 item.setChecked(true);
                 break;
             }
-            case R.id.bottom_nav_favorite:{
+            case R.id.bottom_nav_favorite: {
                 SavedConnectionFragment savedConnectionFragment = new SavedConnectionFragment();
                 FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                 fragmentTransaction.replace(R.id.main_content_frame, savedConnectionFragment, getString(R.string.tag_fragment_saved_connections));
@@ -54,21 +100,21 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 item.setChecked(true);
                 break;
             }
-            case R.id.bottom_nav_messages:{
+            case R.id.bottom_nav_messages: {
                 MessagesFragment messagesFragment = new MessagesFragment();
                 FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.main_content_frame,messagesFragment,getString(R.string.tag_fragment_messages));
+                fragmentTransaction.replace(R.id.main_content_frame, messagesFragment, getString(R.string.tag_fragment_messages));
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
                 item.setChecked(true);
                 break;
             }
         }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
         return false;
-
     }
 
-    private void initializeFragment(){
+    private void initializeFragment() {
         HomeFragment homeFragment = new HomeFragment();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.main_content_frame, homeFragment, getString(R.string.tag_fragment_home));
@@ -76,11 +122,17 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         fragmentTransaction.commit();
     }
 
-    private void isFirstLogin(){
+    private void setNavigationViewListener(){
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    private void isFirstLogin() {
         final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         boolean isFirstLogin = sharedPreferences.getBoolean(PreferencesKey.FIRST_TIME_LOGIN, true);
 
-        if(isFirstLogin){
+        if (isFirstLogin) {
 
             final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
             alertDialogBuilder.setMessage(R.string.welcome_message);
